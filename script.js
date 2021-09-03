b33afc66358fc299478eb951fd211e77dd867c72
xta <ni77ty@gmail.com>
2021-09-03 18:53:51 +0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Не показывает футер, если мы в iframe
 if (window !== window.parent) {
 	const footer = document.querySelector('footer');
-	if (footer) footer.style.visibility = 'hidden';
+	if (footer) footer.style.visibility = 'collapse';
 }
